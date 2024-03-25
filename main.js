d8db62b47aff5be28d089534ef37bf8dfb4ed6ab
Munyangeyoelie <munyangeyoelie38@gmail.com>
2024-03-25 22:27:01 +0300

    validation

--- main.js
+++ main.js
@@ -7,8 +7,12 @@ function checkPassword() {
   if (password.length != 0) {
     if (password != confirmPassword) {
       document.getElementById("message").value = "Passwords do not match";
+      message.textbackgroundColor = "#ff4d4d";
     } else {
       document.getElementById("message").value = "Password match";
+      message.textbackgroundColor = "#3ae374";
     }
+  } else {
+    alert("Please enter a password");
   }
 }
